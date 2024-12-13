1ad5fda73f8caf69c097550e46b8a913bd7a6f1b
Domhnall Boyle <domhnallboyle@gmail.com>
2024-12-13 18:04:58 +0000

    Customisable scheduler args

--- scheduler.py
+++ scheduler.py
@@ -5,14 +5,17 @@ import config
 
 class CustomScheduler(torch.optim.lr_scheduler.LRScheduler):
 
-    def __init__(self, optimiser):
+    def __init__(self, optimiser, d_model=config.d_model, warmup_steps=config.warmup_steps):
+        self.optimiser = optimiser
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
         self.num_steps = 0
         self.lr = None
 
         super().__init__(optimiser)
 
     def get_lr(self):
-        self.lr = (config.d_model ** -0.5) * min(self.num_steps ** -0.5, self.num_steps * (config.warmup_steps ** -1.5))
+        self.lr = (self.d_model ** -0.5) * min(self.num_steps ** -0.5, self.num_steps * (self.warmup_steps ** -1.5))
 
         return [self.lr for _ in range(len(self.optimizer.param_groups))]
 
